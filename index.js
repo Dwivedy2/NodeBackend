d97b390dc3109190dbb8e097255235db23162d6c
Omkareshwar Nath Dwivedy <omkareshwarnath.dwivedy.sbg@gmail.com>
2024-11-14 17:55:16 +0530

    NodeBackend: Setup

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send("omkareshwarnath.com")
+})
+
+app.get('/login', (req, res) => {
+    res.send('<h1>you are not signed up, please sign up</h1>')
+})
+
+app.get('/youtube', (req, res) => {
+    res.send(`<a href="https://www.youtube.com/watch?v=pOV4EjUtl70&list=PLu71SKxNbfoBGh_8p_NS-ZAh6v7HhYqHW&index=2">Youtube Link</a>`)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})

a6c88c32d5d3f76a962c0e553b86a44360e1b870
Omkareshwar Nath Dwivedy <omkareshwarnath.dwivedy.sbg@gmail.com>
2024-11-14 18:15:14 +0530

    NodeBackend: Adding dotenv module

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
@@ -18,6 +19,6 @@ app.get('/youtube', (req, res) => {
     res.send(`<a href="https://www.youtube.com/watch?v=pOV4EjUtl70&list=PLu71SKxNbfoBGh_8p_NS-ZAh6v7HhYqHW&index=2">Youtube Link</a>`)
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
 })
